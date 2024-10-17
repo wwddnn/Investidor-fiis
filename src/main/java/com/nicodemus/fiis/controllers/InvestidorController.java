4b7b22f0a5ab3173f1a250d9d422ddc0b1394dfd
GabrielRodrigues <gabriel.rodrigues@ctw.bmwgroup.com>
2024-10-18 00:29:54 +0100

    Added springdoc-openapi Added Exception Handling

--- src/main/java/com/nicodemus/fiis/controllers/InvestidorController.java
+++ src/main/java/com/nicodemus/fiis/controllers/InvestidorController.java
@@ -1,12 +1,17 @@
 package com.nicodemus.fiis.controllers;
 
-import com.nicodemus.fiis.DTO.InvestidorDTO;
-import com.nicodemus.fiis.services.InvestidorService;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nicodemus.fiis.DTO.InvestidorDTO;
+import com.nicodemus.fiis.services.InvestidorService;
 
 @RestController
 @RequestMapping(value = "/investidores")
@@ -16,8 +21,18 @@ public class InvestidorController {
     private InvestidorService service;
 
     @GetMapping(value = "/{id}")
-    public InvestidorDTO findById(@PathVariable Long id) {
-        return service.findById(id);
-    }
+    public ResponseEntity<InvestidorDTO> findById(@PathVariable Long id) {
+        try {
+            Optional<InvestidorDTO> investidor = service.findById(id);
 
+            if (investidor.isPresent()) {
+                return ResponseEntity.ok(investidor.get());
+            }
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- src/main/java/com/nicodemus/fiis/services/InvestidorService.java
+++ src/main/java/com/nicodemus/fiis/services/InvestidorService.java
@@ -1,13 +1,14 @@
 package com.nicodemus.fiis.services;
 
-import com.nicodemus.fiis.DTO.InvestidorDTO;
-import com.nicodemus.fiis.entities.Investidor;
-import com.nicodemus.fiis.repositories.InvestidorRepository;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import com.nicodemus.fiis.DTO.InvestidorDTO;
+import com.nicodemus.fiis.entities.Investidor;
+import com.nicodemus.fiis.repositories.InvestidorRepository;
 
 @Service
 public class InvestidorService {
@@ -16,17 +17,21 @@ public class InvestidorService {
     private InvestidorRepository repository;
 
     @Transactional(readOnly = true)
-    public InvestidorDTO findById(Long id) {
-        Optional<Investidor> result =  repository.findById(id);
-        Investidor investidor = result.get();
-        InvestidorDTO dto = new InvestidorDTO();
-        dto.setId(investidor.getId());
-        dto.setNome(investidor.getNome());
-        dto.setEmail(investidor.getEmail());
-        dto.setTelefone(investidor.getTelefone());
-        dto.setCorretora(investidor.getCorretora());
+    public Optional<InvestidorDTO> findById(Long id) {
+        Optional<Investidor> result = repository.findById(id);
 
-        return dto;
+        if (result.isPresent()) {
+            Investidor investidor = result.get();
+            InvestidorDTO dto = new InvestidorDTO();
+            dto.setId(investidor.getId());
+            dto.setNome(investidor.getNome());
+            dto.setEmail(investidor.getEmail());
+            dto.setTelefone(investidor.getTelefone());
+            dto.setCorretora(investidor.getCorretora());
+
+            return Optional.ofNullable(dto);
+        }
+        return null;
     }
 
 }
